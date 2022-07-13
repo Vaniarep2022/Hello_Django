9fc9037c785e8640b5d34ddec6c23ab53de115de
baham <bahamut95@mail.ru>
2022-07-13 21:43:58 +0300

    start_files

--- hello_django/hello_django/views.py
+++ hello_django/hello_django/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 def about(request):
     a = 5+6
     return render(request, 'about.html', {'gretting': a})
+
+def reverse(request):
+    user_text=request.GET['username']
+    reverse=user_text[::-1]
+    return render(request,'reverse.html', {'word':reverse})
+
 def home(request):
     return HttpResponse('This is my home')
